0d363ec6336690d2b4a7cee3048b3a607fba8111
denidim <fs900220@gmail.com>
2024-07-05 13:04:11 +0100

    Add new task - CopyBinaryFile

---
+++ StreamsRecap/CopyBinaryFile/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CopyBinaryFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
