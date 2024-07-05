55769968c1e4ffc1420c854e7210f7c2c15791d1
denidim <fs900220@gmail.com>
2024-07-05 13:00:06 +0100

    Change Read/Write to use File-class Add more logic

--- StreamsRecap/LineNumbers/Program.cs
+++ StreamsRecap/LineNumbers/Program.cs
@@ -1,26 +1,55 @@
-﻿namespace LineNumbers
+﻿using System.Text;
+
+namespace LineNumbers
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
-            using (var sr = new StreamReader("input.txt"))
+            //using (var sr = new StreamReader("input.txt"))
+            //{
+            //    string? line = await sr.ReadLineAsync();
+
+            //    using (var writer = new StreamWriter("../../../Output.txt"))
+            //    {
+            //        int counter = 1;
+
+            //        while (line != null)
+            //        {
+            //            await writer.WriteLineAsync($"{counter}. {line}");
+
+            //            counter++;        
+            //            line = await sr.ReadLineAsync();
+            //        }
+            //    }
+            //}
+
+            string inputFilePath = "../../../Input.txt";
+            string outputFilePath = "../../../Output.txt";
+            await ProcessLines(inputFilePath, outputFilePath);
+
+        }
+
+        public static async Task ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = await File.ReadAllLinesAsync(inputFilePath);
+            string processedLines = InsertAndCount(lines);
+            await File.WriteAllTextAsync(outputFilePath,processedLines);
+        }
+
+        private static string InsertAndCount(string[] lines)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < lines.Length; i++)
             {
-                string? line = await sr.ReadLineAsync();
+                int lettersCount = lines[i].Count(x => Char.IsLetter(x));
+                int punctuationsCount = lines[i].Count(x => Char.IsPunctuation(x));
 
-                using (var writer = new StreamWriter("../../../Output.txt"))
-                {
-                    int counter = 1;
-
-                    while (line != null)
-                    {
-                        await writer.WriteLineAsync($"{counter}. {line}");
-
-                        counter++;        
-                        line = await sr.ReadLineAsync();
-                    }
-                }
+                sb.AppendLine($"Line {i + 1}: - {lines[i]} ({lettersCount})({punctuationsCount})");
             }
+
+            return sb.ToString();
         }
     }
 }
