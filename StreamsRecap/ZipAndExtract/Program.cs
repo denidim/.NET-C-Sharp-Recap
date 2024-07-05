d5236c4586076c9ced777c9f2ab30e5e966bb1d8
denidim <fs900220@gmail.com>
2024-07-05 21:30:37 +0100

    Add Open Creat Read Zip Files

--- StreamsRecap/ZipAndExtract/Program.cs
+++ StreamsRecap/ZipAndExtract/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ZipAndExtract
+﻿using System.IO.Compression;
+
+namespace ZipAndExtract
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string inputFile = @"..\..\..\copyMe.png";
+            string zipArchiveFile = @"..\..\..\archive.zip";
+            string extractedFile = @"..\..\..\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using ZipArchive zip = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+
+            zip.CreateEntryFromFile(inputFilePath, "copyMe.png");
+        }
+        
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            using ZipArchive zip = ZipFile.OpenRead(zipArchiveFilePath);
+
+            ZipArchiveEntry current = zip.GetEntry(fileName)!;
+
+            current.ExtractToFile(outputFilePath);
         }
     }
 }
