742cf41ba595f890c11102088405bcb231d2c237
denidim <fs900220@gmail.com>
2024-07-05 20:27:44 +0100

    Add logic for delete create and  copy files in directory

--- StreamsRecap/CopyAllFiles/Program.cs
+++ StreamsRecap/CopyAllFiles/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace CopyAllFiles
+﻿
+namespace CopyAllFiles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string inputPath = @"C:\Users\fs900\Desktop\.NET-C-Sharp-Recap\StreamsRecap\DirectoryTraversal"; //Console.ReadLine()!;
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string outputPath = $@"{desktop}\here";//Console.ReadLine()!;
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        private static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            
+            foreach (var item in files)
+            {
+                string fileName = outputPath + "\\" + Path.GetFileName(item);
+                File.Copy(item, fileName);
+            }
         }
     }
 }
