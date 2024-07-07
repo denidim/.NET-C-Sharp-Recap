17c172bd6b1010a95ccce2a50cee886043521b1c
denidim <fs900220@gmail.com>
2024-07-07 18:13:54 +0100

    Declare Functions

--- FunctionalProgrammingRecap/AppliedArithmetics/Program.cs
+++ FunctionalProgrammingRecap/AppliedArithmetics/Program.cs
@@ -7,6 +7,20 @@
             int[] numbers = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
 
             string action = Console.ReadLine()!;
+
+            Action<int[]> addNumber = x =>
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    x[i] += 1;
+                }
+            };
+
+            Func<int[], int[]> multiply = x => x.Select(x => x *= 2).ToArray();
+
+            Func<int[], int[]> subtract = x => x.Select(x => x -= 1).ToArray();
+
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
         }
     }
 }
