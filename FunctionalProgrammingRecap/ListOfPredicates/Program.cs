1334636e457d4507aabbf6c89d23de9e60c7d4f9
denidim <fs900220@gmail.com>
2024-07-07 22:10:43 +0100

    Create List-Of-Predicate-Exercise

---
+++ FunctionalProgrammingRecap/ListOfPredicates/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
