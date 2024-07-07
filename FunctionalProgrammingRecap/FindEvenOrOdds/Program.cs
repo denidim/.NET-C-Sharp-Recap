40a9917a4b04eeb5537f91200c2c686c7321745a
denidim <fs900220@gmail.com>
2024-07-07 13:54:24 +0100

    Create Find-Even-Or-Odds-Exercise

---
+++ FunctionalProgrammingRecap/FindEvenOrOdds/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindEvenOrOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
