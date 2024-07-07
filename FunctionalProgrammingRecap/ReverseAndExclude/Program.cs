93aca7bd7875cf748040bf146d4d730e632d342a
denidim <fs900220@gmail.com>
2024-07-07 21:46:38 +0100

    Add logic

--- FunctionalProgrammingRecap/ReverseAndExclude/Program.cs
+++ FunctionalProgrammingRecap/ReverseAndExclude/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> collection = Console.ReadLine()!.Split().Select(int.Parse).ToList();
+
+            int n = int.Parse(Console.ReadLine()!);
+
+            Predicate<int> isDivisible = x => x % n == 0;
+
+            Func<List<int>, List<int>> removeFunction = x => x.Where(x => !isDivisible(x)).ToList();
+
+            List<int> result = removeFunction(collection);
+
+            Action<List<int>> reverseAction = x => x.Reverse();
+
+            reverseAction(result);
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
