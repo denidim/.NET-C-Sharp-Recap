5d211fc651a82c37b5a62555810273abab71d1ab
denidim <fs900220@gmail.com>
2024-07-07 13:31:15 +0100

    Create Custom-Min-Function-Exercise

---
+++ FunctionalProgrammingRecap/CustomMinFunction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
