48ea60126b27038cf6ff8e62ac2b103dadaa761f
denidim <fs900220@gmail.com>
2024-07-07 13:18:18 +0100

    Create Knight-Of-honeor-Exercise

---
+++ FunctionalProgrammingRecap/KnightsOfHonor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
