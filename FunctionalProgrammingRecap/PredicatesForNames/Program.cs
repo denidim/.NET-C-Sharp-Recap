27cfe8e59f491368757d92c69862b8e70f5d7a33
denidim <fs900220@gmail.com>
2024-07-07 22:09:05 +0100

    Add function - less or equal

--- FunctionalProgrammingRecap/PredicatesForNames/Program.cs
+++ FunctionalProgrammingRecap/PredicatesForNames/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine()!);
+
+            List<string> names = Console.ReadLine()!.Split().ToList();
+
+
+            Func<string, int, bool> isLessOrEqual = (name, length) => name.Length <= length;
+
+            string[] result = names.Where(x => isLessOrEqual(x,n)).ToArray();
+
+            Console.WriteLine(string.Join("",result));
+
+            //Predicate<string> lessThanOrEqual = x => x.Length <= n;
+
+            //Action<List<string>> action = x => Console.Write(string.Join(" ", x.Where(x => lessThanOrEqual(x))));
+
+            //action(names);
         }
     }
 }
