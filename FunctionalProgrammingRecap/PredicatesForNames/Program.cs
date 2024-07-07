7324a3634163b9be5c84fb8ecd24018556954dd4
denidim <fs900220@gmail.com>
2024-07-07 21:48:02 +0100

    Create Predicate-For-Names-Exercise

---
+++ FunctionalProgrammingRecap/PredicatesForNames/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PredicatesForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
