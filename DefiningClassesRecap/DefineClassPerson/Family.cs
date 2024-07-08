3d6e410047b43df2364724595badb3f5e10cba16
denidim <fs900220@gmail.com>
2024-07-08 20:38:19 +0100

    Add class Family

---
+++ DefiningClassesRecap/DefineClassPerson/Family.cs
@@ -0,0 +1,19 @@
+﻿namespace DefineClassPerson
+{
+    internal class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember() => People.OrderByDescending(x => x.Age).First();
+    }
+}
