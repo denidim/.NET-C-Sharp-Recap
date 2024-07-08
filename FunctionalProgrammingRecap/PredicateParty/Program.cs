a9249a12a892577fab03e9286d2c24b283f5d01b
denidim <fs900220@gmail.com>
2024-07-08 12:15:28 +0100

    Add logic

--- FunctionalProgrammingRecap/PredicateParty/Program.cs
+++ FunctionalProgrammingRecap/PredicateParty/Program.cs
@@ -8,6 +8,64 @@
 
             string input = Console.ReadLine()!;
             
+            while (input != "Party!")
+            {
+                string[] tokens = input.Split(" ");
+                string command = tokens[0];
+                string criteria = tokens[1];
+                string value = tokens[2];
+
+                if (command == "Remove")
+                {
+                    people.RemoveAll(GetPredicate(criteria, value));
+                }
+                else if (command == "Double")
+                {
+                    //people.AddRange(people.FindAll(GetPredicate(criteria,value)));
+
+                    var doubles = people.FindAll(GetPredicate(criteria, value));
+
+                    if(!doubles.Any())
+                    {
+                        input = Console.ReadLine()!;
+                        continue;
+                    }
+
+                    int index = people.FindIndex(GetPredicate(criteria, value));
+
+                    people.InsertRange(index, doubles);
+                }
+
+                input = Console.ReadLine()!;
+            }
+
+            Print(people);
+        }
+        private static Predicate<string> GetPredicate(string criteria, string value)
+        {
+            if (criteria == "StartsWith")
+            {
+                return x => x.StartsWith(value);
+
+            }
+            else if (criteria == "EndsWith")
+            {
+                return x => x.EndsWith(value);
+            }
+            else
+            {
+                return x => x.Length == int.Parse(value);
+            }
+        }
+
+        private static void Print(List<string> people)
+        {
+            if (people.Count == 0)
+            {
+                Console.WriteLine("Nobody is going to the party!");
+                return;
+            }
+            Console.WriteLine($"{string.Join(" ", people)} are going to the party!");
         }
     }
 }
