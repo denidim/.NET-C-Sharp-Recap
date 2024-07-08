88afa8ce09c0d66cceb9ebffb6a4589049e2d6ad
denidim <fs900220@gmail.com>
2024-07-08 19:31:06 +0100

    Create class Person

---
+++ DefiningClassesRecap/DefineClassPerson/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineClassPerson
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name 
+        { 
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+
+        public int Age { get => age; set => age = value; }
+    }
+}
