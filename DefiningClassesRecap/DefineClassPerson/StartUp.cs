e5af0d4f1f787ea57567f26b471dadf2611217a2
denidim <fs900220@gmail.com>
2024-07-08 20:38:30 +0100

    Add logic

--- DefiningClassesRecap/DefineClassPerson/StartUp.cs
+++ DefiningClassesRecap/DefineClassPerson/StartUp.cs
@@ -4,8 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            Console.WriteLine(person.Name);
+            int n = int.Parse(Console.ReadLine()!);
+
+            Family family = new Family();
+
+            Person person;
+
+            for (int i = 0; i <n; i++)
+            {
+                string[] nameAge = Console.ReadLine()!.Split();
+
+                string name = nameAge[0];
+
+                int age = int.Parse(nameAge[1]);
+
+                person = new Person(name,age);
+
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
         }
     }
 }
