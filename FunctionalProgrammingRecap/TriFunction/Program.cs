05fcfdda83c8b137d7560cc728e35d4c7bcb59ef
denidim <fs900220@gmail.com>
2024-07-08 13:46:52 +0100

    Add nested Func logic

--- FunctionalProgrammingRecap/TriFunction/Program.cs
+++ FunctionalProgrammingRecap/TriFunction/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine()!);
+
+            string[] names = Console.ReadLine()!.Split();
+
+            Func<string, int, bool> condition = (name, sum) => name.Sum(x => x) >= sum;
+
+            Func<string[], int, Func<string, int, bool>, string> func = (names, n, condition)
+                => names.Where(x => condition(x, n)).FirstOrDefault()!;
+
+            string name = func(names,n,condition);
+
+            Console.WriteLine(name);
         }
     }
 }
