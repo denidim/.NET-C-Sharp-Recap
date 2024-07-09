1e623d8017d5e6929b191006c76c83bf7625e322
denidim <fs900220@gmail.com>
2024-07-09 15:57:00 +0100

    Create Doubly-Linked-List

---
+++ CustomLinkedList/LinkedList/DoublyLinkedLIst.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkedList
+{
+    public class DoublyLinkedLIst
+    {
+        public Node Head { get; set; }
+
+        public Node Tail { get; set; }
+
+        public void AddFirst(Node node)
+        {
+            // Check first Node
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+            else 
+            {
+                Node previousHed = Head;
+                Head = node;
+                previousHed.Previous = Head;
+                Head.Next = previousHed;
+            }
+        }
+    }
+}
