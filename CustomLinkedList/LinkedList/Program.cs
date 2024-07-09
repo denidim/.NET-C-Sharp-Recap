eae5e321bee7faaf243a58e3a9bd38336f328286
denidim <fs900220@gmail.com>
2024-07-09 15:57:48 +0100

    Add logic

--- CustomLinkedList/LinkedList/Program.cs
+++ CustomLinkedList/LinkedList/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LinkedList
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //LinkedList<int> list = new LinkedList<int>();
+            //LinkedListNode<int> node = list.First;
+            //while (node != null)
+            //{
+            //    Console.WriteLine(node.Value);
+            //    node = node.Next;
+            //}
+
+            var linkedList = new DoublyLinkedLIst();
+
+            linkedList.AddFirst(new Node(1));
+            linkedList.AddFirst(new Node(2));
+            linkedList.AddFirst(new Node(3));
+
+            Node node = linkedList.Head;
+
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+
+
         }
     }
 }
