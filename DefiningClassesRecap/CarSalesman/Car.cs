acbe803a8b0c372b1b01760c35e8df93151829a5
denidim <fs900220@gmail.com>
2024-07-09 11:30:27 +0100

    Create Class Car

---
+++ DefiningClassesRecap/CarSalesman/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSalesman
+{
+    internal class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            Model = model;
+            Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int? weight) : this(model, engine)
+        {
+            Weight = weight;
+        }
+
+        public Car(string model, Engine engine, int? weight, string? color) : this(model, engine, weight)
+        {
+            Color = color;
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int? Weight { get; set; }
+
+        public string? Color { get; set; }
+    }
+}
