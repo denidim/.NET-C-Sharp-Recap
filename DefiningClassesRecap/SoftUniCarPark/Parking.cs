fb37495e18bdd8a953c00d500879e6b896748b8c
denidim <fs900220@gmail.com>
2024-07-09 10:38:42 +0100

    Add Class Parking

---
+++ DefiningClassesRecap/SoftUniCarPark/Parking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniCarPark
+{
+    internal class Parking
+    {
+        private int capacity;
+        private Dictionary<string, Car> cars;
+
+        public Parking(int capacity)
+        {
+            cars = new Dictionary<string, Car>();
+            this.capacity = capacity;
+        }
+
+        public int Count => this.cars.Count;
+
+        public string AddCar(Car car)
+        {
+            string result = string.Empty;
+
+            if (this.cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (this.cars.Count == this.capacity)
+            {
+                return "Parking is full!";
+            }
+
+            this.cars.Add(car.RegistrationNumber, car);
+
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!this.cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+
+            this.cars.Remove(registrationNumber);
+
+            return $"Successfully removed {registrationNumber}";
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return this.cars[registrationNumber];
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                this.cars.Remove(registrationNumber);
+            }
+        }
+    }
+}
