379a0faf01d8b129eccfa13ba8069ac03eea96c0
denidim <fs900220@gmail.com>
2024-07-09 18:38:03 +0100

    Made Node - Generic

--- CustomLinkedList/LinkedList/Node.cs
+++ CustomLinkedList/LinkedList/Node.cs
@@ -1,16 +1,16 @@
 ﻿namespace LinkedList
 {
-    public class Node
+    public class Node<T>
     {
-        public Node(int value)
+        public Node(T value)
         {
             Value = value;
         }
 
-        public int Value { get; set; }
+        public T Value { get; set; }
 
-        public Node Next { get; set; }
+        public Node<T> Next { get; set; }
 
-        public Node Previous { get; set; }
+        public Node<T> Previous { get; set; }
     }
 }
