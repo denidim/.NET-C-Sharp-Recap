8259517d848c2c83e4f63ca7c5c9ec6052bac009
denidim <fs900220@gmail.com>
2024-07-09 10:38:34 +0100

    Add Class Car

---
+++ DefiningClassesRecap/SoftUniCarPark/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniCarPark
+{
+    internal class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNUmber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNUmber;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Make: {this.Make}");
+            stringBuilder.AppendLine($"Model: {this.Model}");
+            stringBuilder.AppendLine($"HorsePower: {this.HorsePower}");
+            stringBuilder.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
