ab394c07fbbedd7a87dad06925c78d80c2f722bf
denidim <fs900220@gmail.com>
2024-07-10 19:18:18 +0100

    Implement Add(int) Implement Count

---
+++ CustomDataStructures/CustomListStackQueue/CustomList.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomListStackQueue
+{
+    internal class CustomList
+    {
+        /*void Add(int element) - Adds the given element to the end of the list
+          int RemoveAt(int index) - Removes the element at the given index
+          bool Contains(int element) - Checks if the list contains the given element returns (True or False)
+          void Swap(int firstIndex, int secondIndex) - Swaps the elements at the given indexes*/
+
+        private int[] _array = new int[2];
+
+        private int _indexer = 0;
+
+        private int _count = 0;
+
+
+        public int Count => this._count;
+
+        public void Add(int element)
+        {
+            _array[_indexer] = element;
+            _indexer++;
+            _count++;
+        }
+
+    }
+}
--- CustomDataStructures/CustomListStackQueue/Program.cs
+++ CustomDataStructures/CustomListStackQueue/Program.cs
@@ -9,24 +9,26 @@
             customList.Add(5);
             // must contain 5
 
-            customList.Add(10);//5, 10
-            int removed = customList.RemoveAt(1);
-            Console.WriteLine(removed); // 10
-            // must contain 5
-
             Console.WriteLine(customList.Count);
-            // 1
 
-            customList.Add(7);//5,7
-            bool contains = customList.Contains(5);
-            Console.WriteLine(contains);
-            //true
+            //customList.Add(10);//5, 10
+            //int removed = customList.RemoveAt(1);
+            //Console.WriteLine(removed); // 10
+            //// must contain 5
 
-            customList.Swap(0, 1);
-            //7 5
+            //Console.WriteLine(customList.Count);
+            //// 1
 
-            Console.WriteLine(customList[0]);//7
-            Console.WriteLine(customList[1]);//5
+            //customList.Add(7);//5,7
+            //bool contains = customList.Contains(5);
+            //Console.WriteLine(contains);
+            ////true
+
+            //customList.Swap(0, 1);
+            ////7 5
+
+            //Console.WriteLine(customList[0]);//7
+            //Console.WriteLine(customList[1]);//5
         }
     }
 }
