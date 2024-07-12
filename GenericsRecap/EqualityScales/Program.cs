2c5dad21018600281a162e0a20da0de4d207eaa5
denidim <fs900220@gmail.com>
2024-07-12 21:48:18 +0100

    Add Equality-Scale-Exercise

---
+++ GenericsRecap/EqualityScales/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EqualityScales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(1, 2);
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
