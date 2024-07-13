c382226ad18bd52c6c2dbaad718ec79fdf7e0e05
denidim <fs900220@gmail.com>
2024-07-13 20:41:19 +0100

    Change ToString to work as inline function

--- GenericsRecap/GenericBoxOfString/Box.cs
+++ GenericsRecap/GenericBoxOfString/Box.cs
@@ -4,12 +4,12 @@ namespace GenericBoxOfString
 {
     internal class Box<T>
     {
-        public List<T> elements { get; set; }
-
         public Box()
         {
             elements = new List<T>();
         }
+
+        public List<T> elements { get; set; }
 
         public void Swap(int firstIndex, int secondIndex)
         {
@@ -18,16 +18,16 @@ namespace GenericBoxOfString
             elements[secondIndex] = curr;
         }
 
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
+        public override string ToString() => string.Join("\n\r", elements.Select(x=>$"{typeof(T)}: {x}"));
+        //{
+        //    var sb = new StringBuilder();
 
-            for (int i = 0; i < elements.Count; i++)
-            {
-                sb.AppendLine($"{elements[i].GetType()}: {elements[i]}");
+        //    for (int i = 0; i < elements.Count; i++)
+        //    {
+        //        sb.AppendLine($"{elements[i].GetType()}: {elements[i]}");
 
-            }
-            return sb.ToString().TrimEnd();
-        }
+        //    }
+        //    return sb.ToString().TrimEnd();
+        //}
     }
 }
