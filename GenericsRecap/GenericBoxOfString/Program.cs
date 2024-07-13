f425e75361eaa3fbf51479e14431d3767900c54d
denidim <fs900220@gmail.com>
2024-07-13 17:58:22 +0100

    Test how da class bahave with string and int Override Box ToString method

--- GenericsRecap/GenericBoxOfString/Program.cs
+++ GenericsRecap/GenericBoxOfString/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            //Test with string
+            //for (int i = 0; i < n; i++)
+            //{
+            //    string s = Console.ReadLine();
+
+            //    Box<string> box = new Box<string>(s);
+
+            //    Console.WriteLine(box.ToString());
+            //}
+
+            //Test with int
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(num);
+
+                Console.WriteLine(box.ToString());
+            }
         }
     }
 }
