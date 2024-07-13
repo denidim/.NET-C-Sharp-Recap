f08043dd8ffef83df49e72984b1aeaed7bac7cda
denidim <fs900220@gmail.com>
2024-07-13 17:57:03 +0100

    Create generic class Box

---
+++ GenericsRecap/GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericBoxOfString
+{
+    internal class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
