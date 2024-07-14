af144418ef4feffd7fb7c06bde4a483d05fcaed2
denidim <fs900220@gmail.com>
2024-07-14 23:22:13 +0100

    Create Generic Threeuple - Tuple with 3 type parameters

--- GenericsRecap/GenericBoxOfString/Program.cs
+++ GenericsRecap/GenericBoxOfString/Program.cs
@@ -4,17 +4,17 @@
     {
         static void Main(string[] args)
         {
-            string[] input = Console.ReadLine().Split();
+            string[] input = Console.ReadLine().Split(" ",4);
 
-            var personInfo = new MyTuple<string, string>($"{input[0]} {input[1]}", input[2]);
+            var personInfo = new Threeuple<string, string, string>($"{input[0]} {input[1]}", input[2], input[3]);
 
             string[] nameBeer = Console.ReadLine().Split();
 
-            var beerInfo = new MyTuple<string, int>(nameBeer[0], int.Parse(nameBeer[1]));
+            var beerInfo = new Threeuple<string, int, string>(nameBeer[0], int.Parse(nameBeer[1]), nameBeer[2]);
 
-            string[] intAndDouble = Console.ReadLine().Split();
+            string[] bankAcc = Console.ReadLine().Split();
 
-            var numbersInfo = new MyTuple<int, double>(int.Parse(intAndDouble[0]), double.Parse(intAndDouble[1]));
+            var numbersInfo = new Threeuple<string, double, string>(bankAcc[0], double.Parse(bankAcc[1]), bankAcc[2]);
 
             Console.WriteLine(personInfo);
             Console.WriteLine(beerInfo);
