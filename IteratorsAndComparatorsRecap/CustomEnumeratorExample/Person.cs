39eb2d6d6cddb25c7b17c64c19f7255da1a7c2dc
denidim <fs900220@gmail.com>
2024-07-15 16:55:15 +0100

    Implement IEnumerable with Custom IEnumerator

--- IteratorsAndComparatorsRecap/CustomEnumeratorExample/Person.cs
+++ IteratorsAndComparatorsRecap/CustomEnumeratorExample/Person.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace CustomEnumeratorExample
 {
-    internal class Person
+    internal class Person : IEnumerable<int>
     {
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int[] Grades { get; set; } = [1, 2, 3];
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new ReversePersonGradesCustomEnumerator(Grades);
+        }
+
+        // Legacy
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
