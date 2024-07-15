7d0346fdfb8846322ac2684d9c892dfcb0fc3bca
denidim <fs900220@gmail.com>
2024-07-15 20:12:45 +0100

    Add Overide ToString in Book class

--- IteratorsAndComparatorsRecap/Library/Book.cs
+++ IteratorsAndComparatorsRecap/Library/Book.cs
@@ -14,5 +14,24 @@
         public int Year { get; set; }
 
         public IEnumerable<string> Authors { get; set; }
+
+        public override string ToString()
+        {
+            string authors = string.Empty;
+
+            if (this.Authors.Count() == 0) 
+            {
+                authors = "Author: n/a";
+            }
+            else if(this.Authors.Count() == 1)
+            {
+                authors = $"Author: {this.Authors.First()}";
+            }
+            else
+            {
+                authors = $"Author: {string.Join("\n\rAuthor: ", this.Authors)}";
+            }
+            return $"Title: {this.Title} Year: {this.Year}\n\r{authors}";
+        }
     }
 }
