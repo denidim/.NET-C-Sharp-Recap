cb17ceea65e85dbc56f49c82c7df3138b64529ca
denidim <fs900220@gmail.com>
2024-07-15 18:12:22 +0100

    Create class BooksLibrary that implement IEnumerable<Book>

---
+++ IteratorsAndComparatorsRecap/Library/BooksLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace Library
+{
+    internal class BooksLibrary : IEnumerable<Book>
+    {
+        private readonly IEnumerable<Book> books;
+
+        public BooksLibrary(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
