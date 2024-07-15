4f0c838dbeca2e4b27b30e251a997bb3c2a3b772
denidim <fs900220@gmail.com>
2024-07-15 16:54:22 +0100

    Create Custom IEnumerator - ReversePersonGradesCustomEnumerator

---
+++ IteratorsAndComparatorsRecap/CustomEnumeratorExample/ReversePersonGradesCustomEnumerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+namespace CustomEnumeratorExample
+{
+    internal class ReversePersonGradesCustomEnumerator : IEnumerator<int>
+    {
+        private int index;
+
+        private int[] items;
+
+        public ReversePersonGradesCustomEnumerator(int[] items)
+        {
+            this.items = items;
+            index = items.Length;
+        }
+
+        public int Current => items[index];
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            index--;
+
+            if (index < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        // legacy
+        object IEnumerator.Current => this.Current;
+    }
+}
