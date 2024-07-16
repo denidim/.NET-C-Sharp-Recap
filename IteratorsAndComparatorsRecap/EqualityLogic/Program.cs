843a3fac2c901b38fafc99455cc79763c108790c
denidim <fs900220@gmail.com>
2024-07-16 18:59:41 +0100

    Create new task Equality Logic

---
+++ IteratorsAndComparatorsRecap/EqualityLogic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EqualityLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
