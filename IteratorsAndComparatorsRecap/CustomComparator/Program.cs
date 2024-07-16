9c890025732b33024e3ed51843626ec3314c4463
denidim <fs900220@gmail.com>
2024-07-16 20:36:02 +0100

    Add Func customComparator

--- IteratorsAndComparatorsRecap/CustomComparator/Program.cs
+++ IteratorsAndComparatorsRecap/CustomComparator/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] ints = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            //even -> odds -> asc -> func
+
+            Func<int, int, int> customComparer = (x, y) =>
+            {
+                return (x % 2 == 0) && (y % 2 != 0)
+                ? -1
+                : (x % 2 != 0) && (y % 2 == 0)
+                ? 1
+                : x > y
+                ? 1
+                : x < y
+                ? -1
+                : 0;
+            };
+            Array.Sort(ints, (x, y) => customComparer(x, y));
+
+            Console.WriteLine(string.Join(", ", ints));
         }
     }
 }
