d20fafa246971ac73f5deafbe0c06cacbc565262
denidim <fs900220@gmail.com>
2024-07-16 14:43:03 +0100

    Add Push method

--- IteratorsAndComparatorsRecap/StackExercise/Stack.cs
+++ IteratorsAndComparatorsRecap/StackExercise/Stack.cs
@@ -8,5 +8,13 @@
         {
             this.elements = elements.ToList();
         }
+
+        public void Push(params T[] elements)
+        {
+            foreach (var item in elements)
+            {
+                this.elements.Add(item);
+            }
+        }
     }
 }
