4a1ab5e1892bb080ea9062c8f2d194916dfede63
denidim <fs900220@gmail.com>
2024-07-16 15:20:02 +0100

    Add internal list for storing stones

--- IteratorsAndComparatorsRecap/Froggy/Lake.cs
+++ IteratorsAndComparatorsRecap/Froggy/Lake.cs
@@ -1,6 +1,12 @@
 ﻿namespace Froggy
 {
     internal class Lake
+        private readonly List<int> stones;
+
+        public Lake(params int[] stones )
+        {
+            this.stones = stones.ToList();
+        }
     {
     }
 }
