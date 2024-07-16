c3acb0282c43c1dc5e29b6142bb80356fdbcf1a5
denidim <fs900220@gmail.com>
2024-07-16 14:42:27 +0100

    Add internal generic list and ctor to pass params into the list

--- IteratorsAndComparatorsRecap/StackExercise/Stack.cs
+++ IteratorsAndComparatorsRecap/StackExercise/Stack.cs
@@ -2,6 +2,11 @@
 {
     internal class Stack<T>
     {
+        private readonly List<T> elements;
 
+        public Stack(params T[] elements)
+        {
+            this.elements = elements.ToList();
+        }
     }
 }
