2d0b3a7cb90d0631e9d233f6f2c4e8f1c4e00657
denidim <fs900220@gmail.com>
2024-07-16 12:33:07 +0100

    #Crated generic class ListyIterator
    Move - should move an internal index position to the next index in the list. The method should return true if it had successfully moved the index and false if there is no next index.
    
    HasNext - should return true, if there is a next index and false if the index is already at the last element of the list.
    
    Print - should print the element at the current internal index. Calling Print on a collection without elements should throw an appropriate exception with the message "Invalid Operation!".

---
+++ IteratorsAndComparatorsRecap/ListyIteratorExercise/ListyIterator.cs
@@ -0,0 +1,27 @@
+﻿namespace ListyIteratorExercise
+{
+    internal class ListyIterator<T>
+    {
+        private List<T> collection = new List<T>();
+
+        private int index = 0;
+
+        public ListyIterator(params T[] collection)
+        {
+            this.collection = collection.ToList();
+        }
+
+        public bool Move() => ++index < this.collection.Count;
+
+        public bool HasNext() => index + 1 < this.collection.Count;
+
+        public void Print()
+        {
+            if (index < 0 && index >= this.collection.Count && collection.Count == 0)
+            {
+                throw new Exception("Invalid Operation!");
+            }
+            Console.WriteLine(collection[index]);
+        }
+    }
+}
