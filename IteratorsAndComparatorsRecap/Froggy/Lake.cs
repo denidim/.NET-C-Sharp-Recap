c82827a31a9ec56dd79d6ae60f5cbf82b3d18cfc
denidim <fs900220@gmail.com>
2024-07-16 15:41:18 +0100

    Implement custom logic for GetEnumerator

--- IteratorsAndComparatorsRecap/Froggy/Lake.cs
+++ IteratorsAndComparatorsRecap/Froggy/Lake.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Froggy
 {
@@ -6,14 +8,31 @@ namespace Froggy
     {
         private readonly List<int> stones;
 
-        public Lake(params int[] stones )
+        public Lake(params int[] stones)
         {
             this.stones = stones.ToList();
         }
 
         public IEnumerator<int> GetEnumerator()
         {
-            throw new NotImplementedException();
+            /* First, the frog must jump on all the stones, which are in even positions in ascending order,
+             * and then on all the odd ones, but in reversed order*/
+
+            for (int i = 0; i < stones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return stones[i];
+                }
+            }
+
+            for (int i = stones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return stones[i];
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
