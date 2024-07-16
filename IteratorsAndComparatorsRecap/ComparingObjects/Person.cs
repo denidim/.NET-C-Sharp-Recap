d3e56dff9507cfa475cdccfed9c4f03337649605
denidim <fs900220@gmail.com>
2024-07-16 18:50:29 +0100

    Implement IComparable<Person>

--- IteratorsAndComparatorsRecap/ComparingObjects/Person.cs
+++ IteratorsAndComparatorsRecap/ComparingObjects/Person.cs
@@ -1,11 +1,35 @@
 ﻿namespace ComparingObjects
 {
-    internal class Person
+    internal class Person : IComparable<Person>
     {
-        public int Age { get; set; }
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
 
         public string Name { get; set; }
 
+        public int Age { get; set; }
+
         public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int names = this.Name.CompareTo(other.Name);
+
+            if(names != 0) return names;
+
+            int age = this.Age.CompareTo(other.Age);
+
+            if(age != 0) return age;
+
+            int towns = this.Town.CompareTo(other.Town);
+
+            if(towns != 0) { return towns; }
+
+            return 0;
+        }
     }
 }
