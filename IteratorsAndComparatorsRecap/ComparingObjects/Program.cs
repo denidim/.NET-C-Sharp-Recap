f31fdb874347003daff9b1d13b5748f2c15dc927
denidim <fs900220@gmail.com>
2024-07-16 15:42:52 +0100

    Create new task Comparing Objects

---
+++ IteratorsAndComparatorsRecap/ComparingObjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ComparingObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
