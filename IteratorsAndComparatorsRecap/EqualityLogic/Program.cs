72a96aec1fea9a28388d7fabd7652a78c89f2060
denidim <fs900220@gmail.com>
2024-07-16 19:33:42 +0100

    Use HashSet Use SortedSet

--- IteratorsAndComparatorsRecap/EqualityLogic/Program.cs
+++ IteratorsAndComparatorsRecap/EqualityLogic/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<Person> hashPerson = new HashSet<Person>();
+            SortedSet<Person> sortedPerson = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+
+                var person = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                hashPerson.Add(person);
+                sortedPerson.Add(person);
+            }
+
+            Console.WriteLine(hashPerson.Count);
+            Console.WriteLine(sortedPerson.Count);
         }
     }
 }
