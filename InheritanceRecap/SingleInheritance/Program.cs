74097b92e370939311a923b11d48640f9b8ba99e
denidim <fs900220@gmail.com>
2024-07-17 12:20:30 +0100

    Create new task Single Inheritance

---
+++ InheritanceRecap/SingleInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SingleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
