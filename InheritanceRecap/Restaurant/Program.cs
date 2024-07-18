808bc468aaf9efe2755ca4b2c4f0277cc744d3c2
denidim <fs900220@gmail.com>
2024-07-18 19:15:01 +0100

    Add logic

--- InheritanceRecap/Restaurant/Program.cs
+++ InheritanceRecap/Restaurant/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var cake = new Dessert("Cake", 5, 250, 1000);
+            var soup = new Starter("Soup", 3, 200);
+            var Fish = new MainDish("Fish", 6, 22);
         }
     }
 }
