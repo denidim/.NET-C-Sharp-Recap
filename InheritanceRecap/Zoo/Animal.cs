09db006cfd23a781a91a2c3bde4dc141d2f6df60
denidim <fs900220@gmail.com>
2024-07-18 14:58:47 +0100

    Create base class Animal

---
+++ InheritanceRecap/Zoo/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace Zoo
+{
+    internal class Animal
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+    }
+}
