71918a86f64ac1daa9bb7754a551d618b4c690f0
denidim <fs900220@gmail.com>
2024-07-18 20:13:10 +0100

    Add subclass TomCat

---
+++ InheritanceRecap/Animals/TomCat.cs
@@ -0,0 +1,14 @@
+﻿namespace Animals
+{
+    public class TomCat : Cat
+    {
+        public TomCat(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+
+        public override void ProduceSound()
+        {
+            Console.WriteLine("MEOW");
+        }
+    }
+}
