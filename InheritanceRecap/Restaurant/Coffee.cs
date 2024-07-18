ed166b70e8a67894b7d9a60f2e321077e3dc2734
denidim <fs900220@gmail.com>
2024-07-18 19:13:10 +0100

    Add subclass Coffee

---
+++ InheritanceRecap/Restaurant/Coffee.cs
@@ -0,0 +1,16 @@
+﻿namespace Restaurant
+{
+    internal class Coffee : HotBeverage
+    {
+        private const double DefaultCoffeeMilliliters = 50;
+
+        private const decimal DefaultCoffeePrice = 3.50m;
+
+        public Coffee(string name, double caffeine = 0) : base(name, DefaultCoffeePrice, DefaultCoffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
