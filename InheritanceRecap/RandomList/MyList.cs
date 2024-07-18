acd4a25510a5ad14f2cb158cdea02faa319707ac
denidim <fs900220@gmail.com>
2024-07-18 14:19:04 +0100

    Extend List<T>  Add GetRandom

---
+++ InheritanceRecap/RandomList/MyList.cs
@@ -0,0 +1,15 @@
+﻿namespace RandomList
+{
+    internal class MyList<T> : List<T>
+    {
+        private Random random = new Random();
+
+        public T GetRandom()
+        {
+            var index = random.Next(0, base.Count);
+
+            return base[index];
+        }
+
+    }
+}
