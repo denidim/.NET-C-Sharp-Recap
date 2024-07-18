d2cf59adadb21fe4959d4a20278721a407029ca8
denidim <fs900220@gmail.com>
2024-07-18 18:03:59 +0100

    Add base class Vehicle

---
+++ InheritanceRecap/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedForSpeed
+{
+    internal class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public int HorsePower { get; set; }
+
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+            Console.WriteLine($"Drive {kilometers}km fuel left = {this.Fuel} litters");
+        }
+    }
+}
