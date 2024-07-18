51189558ccb172cb0b6840f56601957e98370c15
denidim <fs900220@gmail.com>
2024-07-18 19:14:24 +0100

    Add subclass MainDish

---
+++ InheritanceRecap/Restaurant/MainDish.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant
+{
+    internal class MainDish : Food
+    {
+        public MainDish(string name, decimal price, double grams) : base(name, price, grams)
+        {
+        }
+    }
+}
