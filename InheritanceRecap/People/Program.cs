918fbe9f52fd00633bb1d45d95d8f6b02bde1916
denidim <fs900220@gmail.com>
2024-07-18 14:55:00 +0100

    Implement logic

--- InheritanceRecap/People/Program.cs
+++ InheritanceRecap/People/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+
+            Console.WriteLine(child);
         }
     }
 }
