cf3c5aa7961a80f97f126adc7289944f5cda4062
denidim <fs900220@gmail.com>
2024-07-18 20:13:40 +0100

    Add logic

--- InheritanceRecap/Animals/Program.cs
+++ InheritanceRecap/Animals/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Animal animal;
+
+            string input = Console.ReadLine();
+
+            while (input != "Beast!")
+            {
+                string type = input;
+
+                string[] info = Console.ReadLine().Split();
+
+                string name = info[0];
+
+                int age = int.Parse(info[1]);
+
+                string gender = info[2];
+
+                if (type == "Cat")
+                {
+                    if (gender == "Male")
+                    {
+                        animal = new TomCat(name, age, gender);
+                        Console.WriteLine(animal);
+                        animal.ProduceSound();
+                    }
+                    else
+                    {
+                        animal = new Kitten(name, age, gender);
+                        Console.WriteLine(animal);
+                        animal.ProduceSound();
+                    }
+                }
+                else if (type == "Dog")
+                {
+                    animal = new Dog(name, age, gender);
+                    Console.WriteLine(animal);
+                    animal.ProduceSound();
+                }
+                else if (type == "Frog")
+                {
+                    animal = new Frog(name, age, gender);
+                    Console.WriteLine(animal);
+                    animal.ProduceSound();
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
