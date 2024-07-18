a25fcc96b0c11a21e1f7778741a2e61a7049ce76
denidim <fs900220@gmail.com>
2024-07-18 14:24:27 +0100

    Extend Stack<string> Add void method AddRange(params)

---
+++ InheritanceRecap/StackOfStrings/MyStack.cs
@@ -0,0 +1,14 @@
+﻿namespace StackOfStrings
+{
+    internal class MyStack : Stack<string>
+    {
+        public void AddRange(params string[] values)
+        {
+            foreach (string value in values)
+            {
+                base.Push(value);
+            }
+        }
+
+    }
+}
