5b1408d21af3b94296f224fc41aa59878b069cce
denidim <fs900220@gmail.com>
2024-07-18 20:12:37 +0100

    Add base class Animal

---
+++ InheritanceRecap/Animals/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private int age;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender { get; set; }
+
+        public virtual void ProduceSound()
+        {
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
