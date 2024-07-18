52bfd28b5158ea3fbb7cfa34ab0f19ddd92d3baf
denidim <fs900220@gmail.com>
2024-07-18 14:19:38 +0100

    Use extended list

--- InheritanceRecap/RandomList/Program.cs
+++ InheritanceRecap/RandomList/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var list =  new MyList<string>();
+
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            list.Add("d");
+            list.Add("e");
+
+            Console.WriteLine("Random element: " + list.GetRandom());
+
+            Console.WriteLine("Removed random: " + list.RemoveRandom());
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
