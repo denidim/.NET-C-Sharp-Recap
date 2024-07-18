7da91780ab3f88d8f45712eb3e65be2b5d11313f
denidim <fs900220@gmail.com>
2024-07-18 18:05:10 +0100

    Add subclass Motorcycle

---
+++ InheritanceRecap/NeedForSpeed/Motorcycle.cs
@@ -0,0 +1,9 @@
+﻿namespace NeedForSpeed
+{
+    internal class Motorcycle : Vehicle
+    {
+        public Motorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+    }
+}
