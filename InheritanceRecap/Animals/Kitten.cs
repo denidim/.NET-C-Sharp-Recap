27e9cc2d7185f423bf0d4f46e8def64d7a2d7c9d
denidim <fs900220@gmail.com>
2024-07-18 20:13:01 +0100

    Add subclass Kitten

---
+++ InheritanceRecap/Animals/Kitten.cs
@@ -0,0 +1,14 @@
+﻿namespace Animals
+{
+    public class Kitten : Cat
+    {
+        public Kitten(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+
+        public override void ProduceSound()
+        {
+            Console.WriteLine("Meow meow");
+        }
+    }
+}
