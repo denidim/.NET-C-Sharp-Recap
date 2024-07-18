646fc1652d238cf61d6345460f2bc7bebd5cf0c4
denidim <fs900220@gmail.com>
2024-07-18 18:05:43 +0100

    Add subclass CrossMotorcycle

---
+++ InheritanceRecap/NeedForSpeed/CrossMotorcycle.cs
@@ -0,0 +1,9 @@
+﻿namespace NeedForSpeed
+{
+    internal class CrossMotorcycle : Motorcycle
+    {
+        public CrossMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+    }
+}
