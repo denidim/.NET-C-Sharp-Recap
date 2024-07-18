15196af8a4b0a35921584ea89fb57c72c6e1d35b
denidim <fs900220@gmail.com>
2024-07-18 15:12:23 +0100

    Add subclass SoulMaster

---
+++ InheritanceRecap/PlayersAndMonsters/SoulMaster.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayersAndMonsters
+{
+    internal class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+        }
+    }
+}
