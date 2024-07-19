0b7aaa332a6cc627610f5dc76fcac83ac68c4387
denidim <fs900220@gmail.com>
2024-07-19 20:20:45 +0100

    Add new class Team

---
+++ EncapsulationRecap/EncapsulationDemo/Team.cs
@@ -0,0 +1,37 @@
+﻿namespace EncapsulationDemo
+{
+    internal class Team
+    {
+        private string name;
+
+        private List<Person> firstTeam = new List<Person>();
+
+        private List<Person> reserveTeam = new List<Person>();
+
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get => name; set => name = value; }
+
+         IReadOnlyCollection<Person> FirstTeam { get => firstTeam; }
+
+        public IReadOnlyCollection<Person> ReserveTeam { get => reserveTeam; }
+
+        public void AddPlayer(Person person)
+        {
+            if(person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+
+            reserveTeam.Add(person);
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {firstTeam.Count} players.\r\nReserve team has {reserveTeam.Count} players.";
+        }
+    }
+}
