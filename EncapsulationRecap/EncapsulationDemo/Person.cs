f1a536965e01c04b857e6a5f1b168562324ad859
denidim <fs900220@gmail.com>
2024-07-19 16:31:19 +0100

    Add class Person

---
+++ EncapsulationRecap/EncapsulationDemo/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace EncapsulationDemo
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public int Age { get => age; set => age = value; }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} is {this.age} years old.";
+        }
+    }
+}
