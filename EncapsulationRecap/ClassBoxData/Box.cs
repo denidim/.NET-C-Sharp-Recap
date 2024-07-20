3f165ce298368927bf545bdee080a9867afcbb9a
denidim <fs900220@gmail.com>
2024-07-20 20:01:17 +0100

    Add ctor fields and props

--- EncapsulationRecap/ClassBoxData/Box.cs
+++ EncapsulationRecap/ClassBoxData/Box.cs
@@ -2,5 +2,39 @@
 {
     internal class Box
     {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"{this.length.GetType().Name} cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"{this.width.GetType().Name} cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
     }
 }
