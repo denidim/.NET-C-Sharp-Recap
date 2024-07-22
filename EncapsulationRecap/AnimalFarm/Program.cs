9ee4e6689be3dddea2bb1c394ff6d2a924e609cf
denidim <fs900220@gmail.com>
2024-07-22 16:39:59 +0100

    Add logic

--- EncapsulationRecap/AnimalFarm/Program.cs
+++ EncapsulationRecap/AnimalFarm/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace AnimalFarm
 {
-    internal class Program
+    using AnimalFarm.Models;
+    using System;
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
         }
     }
 }
