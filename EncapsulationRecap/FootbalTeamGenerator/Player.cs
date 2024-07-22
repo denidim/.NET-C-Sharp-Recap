dd73c0e26f8d16fef65d647a58695e4935d8deed
denidim <fs900220@gmail.com>
2024-07-22 22:08:51 +0100

    Add class Player

---
+++ EncapsulationRecap/FootbalTeamGenerator/Player.cs
@@ -0,0 +1,35 @@
+﻿namespace FootballTeamGenerator
+{
+    internal class Player : Stats
+    {
+        private string name;
+
+        public Player(string name, params int[] stats)
+            : base(stats[0], stats[1], stats[2], stats[3], stats[4])
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"A {nameof(name)} should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+        public override double SkillLevel()
+        {
+            double all = base.Endurance + base.Dribble + base.Sprint + base.Passing + base.Shooting;
+
+            int countOfPlayerSkills = 5;
+
+            return all / countOfPlayerSkills;
+        }
+    }
+}
