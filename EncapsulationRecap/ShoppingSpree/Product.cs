c162945684c14ffe1d5c8b776d7b01cba4920936
denidim <fs900220@gmail.com>
2024-07-23 13:13:06 +0100

    populate class Product

--- EncapsulationRecap/ShoppingSpree/Product.cs
+++ EncapsulationRecap/ShoppingSpree/Product.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShoppingSpree
+﻿namespace ShoppingSpree
 {
     internal class Product
     {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value), $"{nameof(Name)} Cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get => cost;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(nameof(value), $"{nameof(Cost)} Cannot be negative");
+                }
+                cost = value;
+            }
+        }
     }
 }
