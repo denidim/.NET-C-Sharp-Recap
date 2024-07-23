5890044c4ef1a9e4da724d4864b702f035a02285
denidim <fs900220@gmail.com>
2024-07-23 13:12:54 +0100

    populate class Person

--- EncapsulationRecap/ShoppingSpree/Person.cs
+++ EncapsulationRecap/ShoppingSpree/Person.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShoppingSpree
+﻿namespace ShoppingSpree
 {
     internal class Person
     {
+
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProduct;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(nameof(value), $"{nameof(Name)} Cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(nameof(value), $"{nameof(Money)} Cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public List<Product> BagOfProduct { get => bagOfProduct; set => bagOfProduct = value; }
     }
 }
