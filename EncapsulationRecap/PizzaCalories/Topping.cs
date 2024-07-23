a349f35879e000b94327c2d3ce116fa284c6c6be
denidim <fs900220@gmail.com>
2024-07-23 15:24:13 +0100

    Populate class Topping

--- EncapsulationRecap/PizzaCalories/Topping.cs
+++ EncapsulationRecap/PizzaCalories/Topping.cs
@@ -2,5 +2,49 @@
 {
     internal class Topping
     {
+        private Dictionary<string, double> toppingTypes = new Dictionary<string, double>
+        {
+            { "Meat", 1.2},
+            { "Veggies", 0.8},
+            { "Cheese", 1.1},
+            { "Sauce", 0.9}
+        };
+
+        private string type;
+        private double weight;
+
+        public Topping(string type, double weight)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public string Type
+        {
+            get => type;
+            set
+            {
+                if (!toppingTypes.ContainsKey(value))
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+                type = value;
+            }
+        }
+
+        public double Weight
+        {
+            get => weight;
+            set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new ArgumentException($"{this.Type} weight should be in the range [1..50].");
+                }
+                weight = value;
+            }
+        }
+
+        public double Calories => 2 * weight * toppingTypes[this.Type];
     }
 }
