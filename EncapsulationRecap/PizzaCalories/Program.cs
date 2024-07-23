58d2ea32cda265cd5158560ef8acadd623072b5f
denidim <fs900220@gmail.com>
2024-07-23 15:57:09 +0100

    Add task logic in Main

--- EncapsulationRecap/PizzaCalories/Program.cs
+++ EncapsulationRecap/PizzaCalories/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split();
+                string[] doughInfo = Console.ReadLine().Split();
+
+                string pizzaName = pizzaInfo[1];
+                string doughFlowerType = doughInfo[1];
+                string bakingTechnique = doughInfo[2];
+                double doughWeight = double.Parse(doughInfo[3]);
+
+                Dough dough = new Dough(doughFlowerType, bakingTechnique, doughWeight);
+                Pizza pizza = new Pizza(pizzaName, dough);
+
+                string toppingType = Console.ReadLine();
+
+                while (toppingType != "END")
+                {
+                    string[] toppingInfo = toppingType.Split();
+
+                    string type = toppingInfo[1];
+                    double weight = double.Parse(toppingInfo[2]);
+
+                    Topping topping = new Topping(type, weight);
+
+                    pizza.Add(topping);
+
+                    toppingType = Console.ReadLine();
+                }
+                Console.WriteLine($"{pizza.Name} - {pizza.CalculateCalories:f2} Calories.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
         }
     }
 }
