f9b8f7298d7159d3ea9558f97535120da5cc9e08
denidim <fs900220@gmail.com>
2024-07-27 17:51:46 +0100

    Add task logic

---
+++ InterfacesAndAbstractionRecap/Telephony/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Telephony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneNumbers = Console.ReadLine().Split();
+
+            string[] sites = Console.ReadLine().Split();
+
+            ICallable phone;
+
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                if (phoneNumber.Length < 10)
+                {
+                    phone = new Smartphone();
+                    phone.Call(phoneNumber);
+                }
+                else
+                {
+                    phone = new StationaryPhone();
+
+                    if (!phoneNumber.Any(x => char.IsDigit(x)))
+                    {
+                        Console.WriteLine("Invalid number!");
+                        continue;
+                    }
+                    phone.Call(phoneNumber);
+                }
+            }
+
+            IBrowseable phoneBrowseable;
+
+            foreach (string urls in sites)
+            {
+                phoneBrowseable = new Smartphone();
+                if (urls.Any(x => char.IsDigit(x)))
+                {
+                    Console.WriteLine("Invalid URLs");
+                    continue;
+                }
+                phoneBrowseable.Browse(urls);
+            }
+        }
+    }
+}
