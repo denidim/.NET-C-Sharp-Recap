a093111d801853ae14db27c7f1db3941ec01230e
denidim <fs900220@gmail.com>
2024-07-27 17:44:47 +0100

    Add class Citizen

---
+++ InterfacesAndAbstractionRecap/BorderControl/Citizen.cs
@@ -0,0 +1,12 @@
+﻿namespace BorderControl
+{
+    internal class Citizen : Robot
+    {
+        public Citizen(string name, int age, string id) : base(name, id)
+        {
+            Age = age;
+        }
+
+        public int Age { get; set; }
+    }
+}
