232db58a2829c4c51153b5ffda97451015431e39
denidim <fs900220@gmail.com>
2024-07-27 17:45:04 +0100

    Add class Robot

---
+++ InterfacesAndAbstractionRecap/BorderControl/Robot.cs
@@ -0,0 +1,25 @@
+﻿namespace BorderControl
+{
+    internal class Robot : IDentifiable
+    {
+        public Robot(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name {  get; set; }
+
+        public string Id { get; set; }
+
+        public bool CheckFakeId(string endsWith)
+        {
+            if (this.Id.EndsWith(endsWith))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
