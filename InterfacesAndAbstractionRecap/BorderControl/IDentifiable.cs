ecabfaade06f82f7e3be5a32b2ee5126919dc1ef
denidim <fs900220@gmail.com>
2024-07-27 17:44:36 +0100

    Define interface IDentifiable

---
+++ InterfacesAndAbstractionRecap/BorderControl/IDentifiable.cs
@@ -0,0 +1,11 @@
+﻿namespace BorderControl
+{
+    internal interface IDentifiable
+    {
+        string Name { get; }
+
+        string Id { get; }
+
+        bool CheckFakeId(string endsWith);
+    }
+}
