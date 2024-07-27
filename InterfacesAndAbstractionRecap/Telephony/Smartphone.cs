43c39a95efa45b6f76f201e5d4a985faf3a19947
denidim <fs900220@gmail.com>
2024-07-27 17:50:59 +0100

    Add class Smartphone

---
+++ InterfacesAndAbstractionRecap/Telephony/Smartphone.cs
@@ -0,0 +1,7 @@
+﻿namespace Telephony
+{
+    internal class Smartphone : StationaryPhone, IBrowseable
+    {
+        public void Browse(string url) => Console.WriteLine($"Browsing {url}");
+    }
+}
