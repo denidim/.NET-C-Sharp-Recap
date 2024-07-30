ad0ffbfe3bd2ead5b54f5c8f1ee5cc13fcf60583
denidim <fs900220@gmail.com>
2024-07-30 17:28:46 +0100

    Add interface IMission

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Interfaces/IMission.cs
@@ -0,0 +1,13 @@
+﻿namespace MilitaryElite.Interfaces
+{
+    using MilitaryElite.Enums;
+
+    internal interface IMission
+    {
+        public string CodeName { get; }
+
+        public MissionStateEnum MissionState { get; }
+
+        void CompleteMission();
+    }
+}
