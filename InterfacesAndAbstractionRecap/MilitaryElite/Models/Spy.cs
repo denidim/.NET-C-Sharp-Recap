44e2e76baadea6233cc78150cdb05f2e6da18a07
denidim <fs900220@gmail.com>
2024-07-30 18:25:41 +0100

    Add class Spy

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Spy.cs
@@ -0,0 +1,17 @@
+﻿namespace MilitaryElite.Models
+{
+    using Interfaces;
+
+    internal class Spy : Soldier, ISpy
+    {
+        private int codeNumber;
+
+        public Spy(int id, string firstName, string lastName, int codeNumber)
+            : base(id, firstName, lastName)
+        {
+            this.CodeNumber = codeNumber;
+        }
+
+        public int CodeNumber { get => codeNumber; private set => codeNumber = value; }
+    }
+}
