6d96f1d7d9c6f878e6ef31804c281b4850a296a6
denidim <fs900220@gmail.com>
2024-07-30 18:21:46 +0100

    Add new class Commando

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Commando.cs
@@ -0,0 +1,19 @@
+﻿namespace MilitaryElite.Models
+{
+    using Enums;
+    using Interfaces;
+    using System.Collections.Generic;
+
+    internal class Commando : SpecialisedSoldier, ICommando
+    {
+        private List<IMission> missions = new List<IMission>();
+
+        public Commando(int id, string firstName, string lastName, decimal salary, SoldierCorpEnum corp, ICollection<IMission> missions)
+            : base(id, firstName, lastName, salary, corp)
+        {
+            this.missions = missions.ToList();
+        }
+
+        public ICollection<IMission> Missions => missions.AsReadOnly();
+    }
+}
