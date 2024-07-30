c00f3c01c12f2a30026ae4a529d24a9831348ac7
denidim <fs900220@gmail.com>
2024-07-30 18:28:28 +0100

    Add class Repair

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Repair.cs
@@ -0,0 +1,20 @@
+﻿namespace MilitaryElite.Models
+{
+    using Interfaces;
+
+    internal class Repair : IRepair
+    {
+        private string partName;
+        private int hoursWorked;
+
+        public Repair(string partName, int hoursWorked)
+        {
+            this.PartName = partName;
+            this.HoursWorked = hoursWorked;
+        }
+
+        public string PartName { get => partName; private set => partName = value; }
+
+        public int HoursWorked { get => hoursWorked; private set => hoursWorked = value; }
+    }
+}
