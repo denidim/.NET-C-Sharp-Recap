2e45085401a2363272d31cd09c057a614b028186
denidim <fs900220@gmail.com>
2024-07-30 18:06:38 +0100

    Add class LieutenantGeneral

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/LieutenantGeneral.cs
@@ -0,0 +1,19 @@
+﻿namespace MilitaryElite.Models
+{
+    using Interfaces;
+    using System.Collections.Generic;
+
+    internal class LieutenantGeneral : Private, ILieutenantGeneral
+    {
+        private List<IPrivate> privates = new List<IPrivate>();
+
+        public LieutenantGeneral(int id, string firstName, string lastName, decimal salary, params IPrivate[] privates)
+            : base(id, firstName, lastName, salary)
+        {
+            this.privates = [.. privates];
+        }
+
+        public ICollection<IPrivate> Privates => this.privates.AsReadOnly();
+
+    }
+}
