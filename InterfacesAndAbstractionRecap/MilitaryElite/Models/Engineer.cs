53a3735f8dbee9034e0b011ca71c63cc9c1ebc59
denidim <fs900220@gmail.com>
2024-07-30 18:17:55 +0100

    Add class Engineer

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Engineer.cs
@@ -0,0 +1,19 @@
+﻿namespace MilitaryElite.Models
+{
+    using Interfaces;
+    using Enums;
+    using System.Collections.Generic;
+
+    internal class Engineer : SpecialisedSoldier, IEngineer
+    {
+        private List<IRepair> repairs = new List<IRepair>();
+
+        public Engineer(int id, string firstName, string lastName, decimal salary, SoldierCorpEnum corp, ICollection<IRepair> repairs)
+            : base(id, firstName, lastName, salary, corp)
+        {
+            this.repairs = repairs.ToList();
+        }
+
+        public ICollection<IRepair> Repairs => repairs.AsReadOnly();
+    }
+}
