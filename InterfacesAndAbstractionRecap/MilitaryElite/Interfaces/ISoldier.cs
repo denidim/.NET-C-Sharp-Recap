bd7fd5378d2650434c99b2a88876fed47b836fc6
denidim <fs900220@gmail.com>
2024-07-30 17:05:20 +0100

    Add interface ISoldier

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Interfaces/ISoldier.cs
@@ -0,0 +1,11 @@
+﻿namespace MilitaryElite.Interfaces
+{
+    internal interface ISoldier
+    {
+        public int Id { get; }
+
+        public string FirstName { get; }
+
+        public string LastName { get; }
+    }
+}
