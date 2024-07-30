4cb9ac0d75ca6e26bbad0b5be3c1060e3a4c61b1
denidim <fs900220@gmail.com>
2024-07-30 18:33:46 +0100

    Add class Mission

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Mission.cs
@@ -0,0 +1,27 @@
+﻿namespace MilitaryElite.Models
+{
+    using Enums;
+    using Interfaces;
+
+    internal class Mission : IMission
+    {
+        private MissionStateEnum missionState;
+
+        private string codeName;
+
+        public Mission(MissionStateEnum missionState, string codeName)
+        {
+            this.MissionState = missionState;
+            this.CodeName = codeName;
+        }
+
+        public MissionStateEnum MissionState { get => missionState; private set => missionState = value; }
+
+        public string CodeName { get => codeName; private set => codeName = value; }
+
+        public void CompleteMission()
+        {
+            this.missionState = MissionStateEnum.Finished;
+        }
+    }
+}
