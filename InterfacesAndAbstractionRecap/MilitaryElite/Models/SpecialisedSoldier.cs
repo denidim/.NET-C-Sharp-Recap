330d3cabe105329e89c55f7a00279a9c51632132
denidim <fs900220@gmail.com>
2024-07-30 18:10:15 +0100

    Add class SpecialisedSoldier

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/SpecialisedSoldier.cs
@@ -0,0 +1,16 @@
+﻿namespace MilitaryElite.Models
+{
+    using Enums;
+    using Interfaces;
+
+    internal abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
+    {
+        public SpecialisedSoldier(int id, string firstName, string lastName, decimal salary, SoldierCorpEnum corp)
+            : base(id, firstName, lastName, salary)
+        {
+            this.Corp = corp;
+        }
+
+        public SoldierCorpEnum Corp { get; }
+    }
+}
