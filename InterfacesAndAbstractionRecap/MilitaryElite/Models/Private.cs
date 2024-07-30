8c3e7d81e052d3980dce7518d9950f32e28ebcc2
denidim <fs900220@gmail.com>
2024-07-30 17:55:26 +0100

    Add class Private

---
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Private.cs
@@ -0,0 +1,16 @@
+﻿using MilitaryElite.Interfaces;
+
+namespace MilitaryElite.Models
+{
+    internal class Private : Soldier, IPrivate
+    {
+        private decimal salary;
+
+        public Private(int id, string firstName, string lastName, decimal salary) : base(id, firstName, lastName)
+        {
+            this.Salary = salary;
+        }
+
+        public decimal Salary { get => salary; private set => salary = value; }
+    }
+}
