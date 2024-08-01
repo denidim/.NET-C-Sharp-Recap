0acc3f6782a0f358eb9f40e13d012e83977fa0b1
denidim <fs900220@gmail.com>
2024-08-01 16:17:46 +0100

    override ToString in base class SpecialisedSoldier

--- InterfacesAndAbstractionRecap/MilitaryElite/Models/SpecialisedSoldier.cs
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/SpecialisedSoldier.cs
@@ -2,15 +2,27 @@
 {
     using Enums;
     using Interfaces;
+    using System.Text;
 
     internal abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
     {
-        public SpecialisedSoldier(int id, string firstName, string lastName, decimal salary, SoldierCorpEnum corp)
+        protected SpecialisedSoldier(int id, string firstName, string lastName, decimal salary, SoldierCorpEnum corp)
             : base(id, firstName, lastName, salary)
         {
             this.Corp = corp;
         }
 
         public SoldierCorpEnum Corp { get; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+
+            sb.AppendLine($"Corps: {this.Corp}");
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
