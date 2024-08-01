0032f65be925ad9382f5938bfdd4b5d3a224d84d
denidim <fs900220@gmail.com>
2024-08-01 16:33:18 +0100

    override ToSting

--- InterfacesAndAbstractionRecap/MilitaryElite/Models/Commando.cs
+++ InterfacesAndAbstractionRecap/MilitaryElite/Models/Commando.cs
@@ -3,6 +3,7 @@
     using Enums;
     using Interfaces;
     using System.Collections.Generic;
+    using System.Text;
 
     internal class Commando : SpecialisedSoldier, ICommando
     {
@@ -15,5 +16,25 @@
         }
 
         public ICollection<IMission> Missions => missions.AsReadOnly();
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+
+
+            if (this.Missions.Count != 0)
+            {
+                sb.AppendLine($"Missions:");
+                sb.Append($"  {string.Join($"{Environment.NewLine}  ", this.Missions)}");
+            }
+            else
+            {
+                sb.Append("Missions:");
+            }
+
+            return sb.ToString();
+        }
     }
 }
